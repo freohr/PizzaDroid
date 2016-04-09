bb2f18dd3498b22afc1737cde60b5c9fb953eeac
Stephen Faure <stephen.faure@netapsys.fr>
2016-04-09 23:26:29 +0200

    travail sur affichage choix des boissons

--- www/js/boisson.js
+++ www/js/boisson.js
@@ -1,33 +1,59 @@
+import * as _ from "./libs/underscore-min";
 /**
  * Created by stephen on 09/04/2016.
  */
 
-function initializeDrinksList() {
-
-}
-
-var app = {
-    initialize : function () {
-        initializeDrinksList();
+var app;
+app = {
+    initialize: function () {
+        this.initDrinkList();
     },
-    
-    bindEvents : function() {
+
+    bindEvents: function () {
 
     },
 
-    addDrink:function (){
+    addDrink: function () {
         //drink
         var drinkList = JSON.parse(localStorage.getItem("drinkList"));
         var tableNumberStr = localStorage.getItem("tableNumber");
         var tableNumber = parseInt(tableNumberStr);
-        var drink = {"name":"pizzaName","table":tableNumber,"price":"pizzaPrice"};
+        var drink = {"name": "pizzaName", "table": tableNumber, "price": "pizzaPrice"};
         drinkList.push(drink);
-        localStorage.setItem("drinkList",JSON.stringify(drinkList));
+        localStorage.setItem("drinkList", JSON.stringify(drinkList));
         //drink number
         var drinkNumberStr = localStorage.getItem("drinkNumber");
-        drinkNumber = parseInt(drinkNumberStr)+1;
-        localStorage.setItem("drinkNumber",drinkNumber);
+        drinkNumber = parseInt(drinkNumberStr) + 1;
+        localStorage.setItem("drinkNumber", drinkNumber);
     },
+
+    chooseDrink : function(event) {
+
+    },
+
+    initDrinkList: function () {
+
+        var drinkList = JSON.parse(localStorage.getItem("drinkList"));
+
+        var ul = document.createElement('ul');
+
+        _.each(drinkList, function (element, index) {
+            var li = document.createElement('li');
+
+            li.className = 'drink ' + index;
+
+            var button = document.createElement('button');
+
+            button.className = li.className;
+
+            li.appendChild(button);
+
+            ul.appendChild(li);
+        });
+        
+        var content = document.querySelector('#content');
+        content.appendChild(ul);
+    }
 };
 
 app.initialize();
--- www/js/summary.js
+++ www/js/summary.js
@@ -2,101 +2,115 @@
  * Created by stephen on 09/04/2016.
  */
 
-function generateCouvertsList(nbCouverts) {
-    var ul = document.createElement('ul');
-    ul.id = 'pizzaList';
 
-    for (var i = 0; i < Math.min(nbCouverts, 10); i++) {
-        var li = document.createElement('li');
-        li.className = 'couvert ' + i;
-
-        var span = document.createElement('span');
-        span.className = 'couvertItem';
-
-        var buttonPizza = document.createElement('button');
-        buttonPizza.className = 'pizza ' + i;
-        buttonPizza.innerHTML = 'Choisir une pizza';
-
-        span.appendChild(buttonPizza);
-
-        var buttonBoisson = document.createElement('button');
-        buttonBoisson.className = 'boisson ' + i;
-        buttonBoisson.innerHTML = 'Choisir une boisson';
-
-        span.appendChild(buttonBoisson);
-
-        li.appendChild(span);
-
-        ul.appendChild(li);
-    }
-
-    return ul;
-}
 
 
 var app = {
-    
-    initialize : function () {
+
+    initialize: function () {
         app.generateCouvertsummary();
     },
 
-    bindEvents : function () {
+    bindEvents: function () {
 
     },
 
-    selectPizza : function(event) {
+    selectPizza: function (event) {
+
+        event.target;
 
     },
 
-    selectBoisson : function(event) {
+    selectBoisson: function (event) {
 
     },
 
-    generateCouvertsummary: function() {
+    generateCouvertsList: function (nbCouverts) {
+        var ul = document.createElement('ul');
+        ul.id = 'pizzaList';
+
+        for (var i = 0; i < Math.min(nbCouverts, 10); i++) {
+            var li = document.createElement('li');
+            li.className = 'couvert ' + i;
+
+            var span = document.createElement('span');
+            span.className = 'couvertItem';
+
+            var buttonPizza = document.createElement('button');
+            buttonPizza.className = 'pizza ' + i;
+            buttonPizza.innerHTML = 'Choisir une pizza';
+
+            span.appendChild(buttonPizza);
+
+            var buttonBoisson = document.createElement('button');
+            buttonBoisson.className = 'boisson ' + i;
+            buttonBoisson.innerHTML = 'Choisir une boisson';
+
+            span.appendChild(buttonBoisson);
+
+            li.appendChild(span);
+
+            ul.appendChild(li);
+        }
+
+        return ul;
+    },
+
+    generateCouvertsummary: function () {
         var qty = localStorage.getItem('qty');
 
 
-        var ul = generateCouvertsList(qty);
+        var ulPizza = this.getPizzas(qty);
+        var ulDrinks = this.getDrinks();
 
         var summary = document.querySelector('#summary');
         summary.appendChild(ul);
     },
-    
-    getPizzas:function(){
+
+    getPizzas: function () {
         var tableNumberStr = localStorage.getItem("tableNumber");
+
+        // numero table courante
         var tableNumber = parseInt(tableNumberStr);
+
+        // liste menu pizzas
         var pizzaList = JSON.parse(localStorage.getItem("pizzaList"));
+
+        // qty de pizzas
         var pizzaNumberStr = localStorage.getItem("pizzaNumber");
         pizzaNumber = parseInt(pizzaNumberStr);
-        for (i=0;i<pizzaNumber;i++){
+
+        var ulPizza = document.createElement('ul');
+
+        for (i = 0; i < pizzaNumber; i++) {
             console.log(pizzaList[i]);
-            if(pizzaList[i].tableNumber=tableNumber){
-                var node = document.createElement("LI");                 // Create a <li> node
+            if (pizzaList[i].tableNumber = tableNumber) {
+                var node = document.createElement("li");                 // Create a <li> node
                 var pizzaName = pizzaList[i].name;
                 var textnode = document.createTextNode(pizzaName);         // Create a text node
                 node.appendChild(textnode);                              // Append the text to <li>
-                document.getElementById("pizzaList").appendChild(node);
-            }else{
+                ulPizza.appendChild(node);
+            } else {
                 console.log("nope");
             }
         }
     },
 
-    getDrinks:function(){
+    getDrinks: function () {
         var tableNumberStr = localStorage.getItem("tableNumber");
         var tableNumber = parseInt(tableNumberStr);
         var drinkList = JSON.parse(localStorage.getItem("drinkList"));
         var drinkNumberStr = localStorage.getItem("drinkNumber");
         drinkNumber = parseInt(drinkNumberStr);
-        for (i=0;i<drinkNumber;i++){
+        for (i = 0; i < drinkNumber; i++) {
             console.log(pizzaList[i]);
-            if(drinkList[i].tableNumber=tableNumber){
+            if (drinkList[i].tableNumber = tableNumber) {
                 var node = document.createElement("LI");                 // Create a <li> node
                 var drinkName = drinkList[i].name;
                 var textnode = document.createTextNode(drinkName);         // Create a text node
                 node.appendChild(textnode);                              // Append the text to <li>
                 document.getElementById("drinkList").appendChild(node);
-            }else{
+            } else {
                 console.log("nope");
             }
         }
