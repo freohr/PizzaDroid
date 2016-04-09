e5e0f36ce5f3872b8bf9c9bd114b57ba0a4cf0d1
Rémy LHERAULT <remy.lherault@gmail.com>
2016-04-09 18:55:39 +0200

    Update choix_pizza.js

--- www/js/choix_pizza.js
+++ www/js/choix_pizza.js
@@ -5,6 +5,22 @@
 var app = {
     initialize : function () {
 
+    },
+    
+    addPizza:function (){
+    //pizza
+    console.log(localStorage.getItem("pizzaList"));
+    var pizzaList = JSON.parse(localStorage.getItem("pizzaList"));
+    var pizza = {"name":"pizzaName","price":"pizzaPrice"};
+    pizzaList.push(pizza);
+    localStorage.setItem("pizzaList",JSON.stringify(pizzaList));
+    console.log(pizzaList);
+    //pizza number
+    var pizzaNumberStr = localStorage.getItem("pizzaNumber");
+    console.log(pizzaNumberStr);
+    pizzaNumber = parseInt(pizzaNumberStr)+1;
+    console.log(pizzaNumber);
+    localStorage.setItem("pizzaNumber",pizzaNumber);
     }
 };
 
