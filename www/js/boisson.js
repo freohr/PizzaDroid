e40321467c5b89ff4705a7e149fbd44f54f18ebb
Stephen Faure <stephen.faure@netapsys.fr>
2016-04-09 19:32:59 +0200

    ajout underscore.js

--- www/js/boisson.js
+++ www/js/boisson.js
@@ -2,10 +2,20 @@
  * Created by stephen on 09/04/2016.
  */
 
+function initializeDrinksList() {
+
+}
+
 var app = {
     initialize : function () {
+        initializeDrinksList();
+    },
 
-    }
+    bindEvents : function() {
+
+    },
+
+    
 };
 
 app.initialize();
--- www/js/summary.js
+++ www/js/summary.js
@@ -2,6 +2,38 @@
  * Created by stephen on 09/04/2016.
  */
 
+function generateCouvertsList(nbCouverts) {
+    var ul = document.createElement('ul');
+    ul.id = 'pizzaList';
+
+    for (var i = 0; i < Math.min(nbCouverts, 10); i++) {
+        var li = document.createElement('li');
+        li.className = 'couvert ' + i;
+
+        var span = document.createElement('span');
+        span.className = 'couvertItem';
+
+        var buttonPizza = document.createElement('button');
+        buttonPizza.className = 'pizza ' + i;
+        buttonPizza.innerHTML = 'Choisir une pizza';
+
+        span.appendChild(buttonPizza);
+
+        var buttonBoisson = document.createElement('button');
+        buttonBoisson.className = 'boisson ' + i;
+        buttonBoisson.innerHTML = 'Choisir une boisson';
+
+        span.appendChild(buttonBoisson);
+
+        li.appendChild(span);
+
+        ul.appendChild(li);
+    }
+
+    return ul;
+}
+
+
 var app = {
     
     initialize : function () {
@@ -16,37 +48,13 @@ var app = {
 
     },
 
+    selectBoisson : function(event) {
+
+    },
+
     generateCouvertsummary: function() {
         var qty = localStorage.getItem('qty');
 
-        function generateCouvertsList(nbCouverts) {
-            var ul = document.createElement('ul');
-            ul.id = 'pizzaList';
-
-            for (var i = 0; i < Math.min(nbCouverts, 10); i++) {
-                var li = document.createElement('li');
-                li.className = 'couvert ' + i;
-
-                var span = document.createElement('span');
-                span.className = 'couvertItem';
-
-                var buttonPizza = document.createElement('button');
-                buttonPizza.className = 'pizza ' + i;
-
-                span.appendChild(buttonPizza);
-
-                var buttonBoisson = document.createElement('button');
-                buttonBoisson.className = 'boisson ' + i;
-
-                span.appendChild(buttonBoisson);
-
-                li.appendChild(span);
-
-                ul.appendChild(li);
-            }
-
-            return ul;
-        }
 
         var ul = generateCouvertsList(qty);
 
