efd8b4f9714377e89f8ca460b6b7c21d446148aa
Rémy LHERAULT <remy.lherault@gmail.com>
2016-04-09 19:04:37 +0200

    Update choix_pizza.js

--- www/js/choix_pizza.js
+++ www/js/choix_pizza.js
@@ -9,18 +9,18 @@ var app = {
     
     addPizza:function (){
     //pizza
-    console.log(localStorage.getItem("pizzaList"));
-    var pizzaList = JSON.parse(localStorage.getItem("pizzaList"));
-    var pizza = {"name":"pizzaName","price":"pizzaPrice"};
-    pizzaList.push(pizza);
-    localStorage.setItem("pizzaList",JSON.stringify(pizzaList));
-    console.log(pizzaList);
-    //pizza number
-    var pizzaNumberStr = localStorage.getItem("pizzaNumber");
-    console.log(pizzaNumberStr);
-    pizzaNumber = parseInt(pizzaNumberStr)+1;
-    console.log(pizzaNumber);
-    localStorage.setItem("pizzaNumber",pizzaNumber);
+        console.log(localStorage.getItem("pizzaList"));
+        var pizzaList = JSON.parse(localStorage.getItem("pizzaList"));
+        var tableNumberStr = localStorage.getItem("tableNumber");
+        var tableNumber = parseInt(tableNumberStr);
+        console.log(tableNumberStr);
+        var pizza = {"name":"pizzaName","table":tableNumber,"price":"pizzaPrice"};
+        pizzaList.push(pizza);
+        localStorage.setItem("pizzaList",JSON.stringify(pizzaList));
+        //pizza number
+        var pizzaNumberStr = localStorage.getItem("pizzaNumber");
+        pizzaNumber = parseInt(pizzaNumberStr)+1;
+        localStorage.setItem("pizzaNumber",pizzaNumber);
     }
 };
 
